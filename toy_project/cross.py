fcbe132f45edb5b721ff55c968b693804bc7f2cb
GustavoMattosLopes <gustavomattoslopes01@gmail.com>
2025-05-27 21:23:17 -0300

    cleaning data

--- toy_project/cross.py
+++ toy_project/cross.py
@@ -3,11 +3,28 @@ import pandas as pd
 data = pd.read_csv("dados_filtrados.csv")
 cdb = pd.read_csv("bares_cdb2025.csv")
 
-missing_addresses = [
-    address for address in cdb["Endereço"]
-    if address not in data["ENDERECO_COMPLETO"].values
-]
+data["NOME_FANTASIA"] = data["NOME_FANTASIA"].str.upper().str.strip()
+data["ENDERECO_COMPLETO"] = data["ENDERECO_COMPLETO"].str.upper().str.strip()
 
-print("Endereços não encontrados:")
-for addr in missing_addresses:
-    print(addr)
+cdb["Nome"] = cdb["Nome"].str.upper().str.strip()
+cdb["Endereço"] = cdb["Endereço"].str.upper().str.strip()
+
+nomes_ref = set(data["NOME_FANTASIA"])
+enderecos_ref = set(data["ENDERECO_COMPLETO"])
+
+nao_encontrados = []
+
+for _, row in cdb.iterrows():
+    nome = row["Nome"]
+    endereco = row["Endereço"]
+
+    if (endereco not in enderecos_ref):
+        nao_encontrados.append({
+            "Nome": nome,
+            "Endereço": endereco
+        })
+
+for item in nao_encontrados:
+    print(f'{item["Nome"]} - {item["Endereço"]}')
+
+pd.DataFrame(nao_encontrados).to_csv("nao_encontrados.csv", index=False, encoding="utf-8-sig")
