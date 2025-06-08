e36fea643cb4d22bd5193454da707abb6c6d0e1d
Gabriel Vieira <gabrel.vieira@outlook.com>
2025-06-08 16:30:34 -0400

    minor changes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ marker_red = {
 }
 
 marker_gray = {
-    "iconUrl": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png",
+    "iconUrl": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
     "shadowUrl": "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
     "iconSize": [25, 41],
     "iconAnchor": [12, 41],
